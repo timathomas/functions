243710fee9fe3bf712353829fd1b63fb4cee740b
Tim Thomas <timthomas@berkeley.edu>
2023-10-16 09:49:16 -0700

    add qs script

--- convert_to_fst.R
+++ convert_to_fst.R
@@ -7,12 +7,12 @@ librarian::shelf(tidyverse, data.table, sf, qs, fst)
 setwd("~/Downloads/data")
 
 #
-# For csv files
+# Batch convert csv files to .fst
 # --------------------------------------------------------------------------
 csv_files <- list.files(pattern = "\\.csv$", ignore.case=TRUE, include.dirs = TRUE, recursive = TRUE)
 
 map(csv_files, function(file){
-x <- fread(file)
+x <- read_csv(file)
 unlink(paste0(substr(file, 1, nchar(file) -4), ".*"))
 write.fst(
     x, 
@@ -20,7 +20,20 @@ write.fst(
 })
 
 #
-# for gpkq files
+# Batch convert csv files to .qs
+# --------------------------------------------------------------------------
+csv_files <- list.files(pattern = "\\.csv$", ignore.case=TRUE, include.dirs = TRUE, recursive = TRUE)
+
+map(csv_files, function(file){
+x <- read_csv(file)
+unlink(paste0(substr(file, 1, nchar(file) -4), ".*"))
+qsave(
+    x, 
+    paste0(substr(file, 1, nchar(file) -4), ".qs"))
+})
+
+#
+# for .gpkq files (replace gpkg to other spatial formats as needed)
 # --------------------------------------------------------------------------
 gpkg_files <- list.files(pattern = "\\.gpkg$", ignore.case=TRUE, include.dirs = TRUE, recursive = TRUE)
 
